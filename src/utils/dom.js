13a7adf6411d109cb608789496791ccd05fdeb0b
icmx <32983315+icmx@users.noreply.github.com>
2021-02-11 19:01:46 +1000

    add dom utils

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -5,4 +5,40 @@
 export const $ = (selector, parent = window.document) =>
   parent.querySelector(selector);
 
-export default { $ };
+/**
+ * Shorthand to standard `setAttribute`, or `removeAttribute` if value
+ * is omitted.
+ */
+export const attr = (element) => (name, value) => {
+  if (!value) {
+    element.removeAttribute(name);
+  } else {
+    element.setAttribute(name, value);
+  }
+};
+
+/**
+ * Shorthand to standard `classList.toggle`.
+ */
+export const toggleClass = (element) => (token, force) => {
+  element.classList.toggle(token, force);
+};
+
+/**
+ * Shorthand to standard textContent property, done as a function.
+ */
+export const text = (element) => (string) => {
+  element.textContent = string;
+};
+
+/**
+ * Shorthand to standard `removeChild` in a loop to remove all the
+ * `element` children.
+ */
+export const empty = (element) => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
+
+export default { $, attr, toggleClass, text, empty };
