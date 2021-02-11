f502ec295e420d2fd2175aad3d9dcd7af7a3a625
icmx <32983315+icmx@users.noreply.github.com>
2021-02-11 19:02:45 +1000

    implement basic comic loading for app

--- src/app.js
+++ src/app.js
@@ -1,19 +1,55 @@
-import { $ } from '~/utils/dom';
+import _ from '~/utils/common';
+import { $, attr, toggleClass, text, empty } from '~/utils/dom';
 
 import '~/app.css';
+import comics from './api/comics';
 
 const App = (appElement) => {
   function render() {
-    appElement.classList.toggle('xv-app--ready', state.ready);
+    toggleClass(appElement)('xv-app--ready', state.ready);
+
+    if (state.comic) {
+      attr(figureElement)('href', `${state.comic.alt} (click for original)`);
+      attr(imageElement)('src', `${state.comic.img}`);
+    } else {
+      attr(figureElement)('href', null);
+      attr(imageElement)('src', null);
+    }
   }
 
   function listen() {
-    // ---
+    window.addEventListener('load', handleLocationChange);
+    window.addEventListener('hashchange', handleLocationChange);
   }
 
   function update(change = {}) {
     state = { ...state, ...change };
     render();
+  }
+
+  function handleLocationChange() {
+    update({ loading: true, error: false, comic: undefined });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    const hash = window.location.hash.substring(1);
+
+    if (hash === '') {
+      comics.current().then(setComic).catch(setError);
+    } else {
+      const num = _.int(hash);
+      comics.get(num).then(setComic).catch(setError);
+    }
+  }
+
+  function setComic(comic) {
+    update({ loading: false, error: false, comic: comic });
+
+    return comic;
+  }
+
+  function setError(error) {
+    console.error(error);
+    update({ loading: false, error: true, comic: undefined });
   }
 
   const firstButton = $('.button--first');
@@ -33,6 +69,10 @@ const App = (appElement) => {
 
   let state = {
     ready: false,
+    loading: false,
+    error: false,
+    num: -1,
+    comic: undefined,
   };
 
   listen();
