9a062dd816dd0bbeecc1f74615bc0ee74a4a6a4e
icmx <32983315+icmx@users.noreply.github.com>
2021-02-11 20:13:52 +1000

    add basic buttons events and state display

--- src/app.js
+++ src/app.js
@@ -2,15 +2,30 @@ import _ from '~/utils/common';
 import { $, attr, toggleClass, text, empty } from '~/utils/dom';
 
 import '~/app.css';
-import comics from './api/comics';
+import comics, { random } from './api/comics';
 
 const App = (appElement) => {
   function render() {
+    console.info(state);
+
     toggleClass(appElement)('xv-app--ready', state.ready);
+    toggleClass(appElement)('xv-app--loading', state.loading);
+    toggleClass(appElement)('xv-app--error', state.error);
 
     if (state.comic) {
-      attr(figureElement)('title', `${state.comic.alt} (click for original)`);
+      attr(figureElement)(
+        'title',
+        `${state.comic.alt} (click for original)`
+      );
       attr(imageElement)('src', `${state.comic.img}`);
+
+      text(headElement)(`${state.comic.head}`);
+      text(leadElement)(`“${state.comic.lead}“`);
+
+      attr(linkElement)('href', state.comic.href);
+      text(linkElement)(`xkcd #${state.comic.num}`);
+
+      text(dateElement)(`${state.comic.date}`);
     } else {
       attr(figureElement)('title', null);
       attr(imageElement)('src', null);
@@ -20,6 +35,30 @@ const App = (appElement) => {
   function listen() {
     window.addEventListener('load', handleLocationChange);
     window.addEventListener('hashchange', handleLocationChange);
+
+    firstButton.addEventListener('click', () => {
+      window.location.hash = 1;
+    });
+
+    previousButton.addEventListener('click', () => {
+      if (state.comic) {
+        window.location.hash = state.comic.num - 1;
+      }
+    });
+
+    randomButton.addEventListener('click', () => {
+      throw new Error(``);
+    });
+
+    nextButton.addEventListener('click', () => {
+      if (state.comic) {
+        window.location.hash = state.comic.num + 1;
+      }
+    });
+
+    currentButton.addEventListener('click', () => {
+      window.location.hash = '';
+    });
   }
 
   function update(change = {}) {
@@ -61,11 +100,11 @@ const App = (appElement) => {
   const figureElement = $('.figure');
   const imageElement = $('.figure__image', figureElement);
 
-  const headElement = $('details__head');
-  const leadElement = $('details__lead');
-  const linkElement = $('details__link');
-  const dateElement = $('details__date');
-  const bodyElement = $('details__body');
+  const headElement = $('.details__head');
+  const leadElement = $('.details__lead');
+  const linkElement = $('.details__link');
+  const dateElement = $('.details__date');
+  const bodyElement = $('.details__body');
 
   let state = {
     ready: false,
