122614ae4f042c5d48243d6e34a813855dc6ca5a
icmx <32983315+icmx@users.noreply.github.com>
2021-05-12 16:03:39 +1000

    fix minor modal issues (related to #61, #62)

--- src/app/features/about/AboutView.js
+++ src/app/features/about/AboutView.js
@@ -4,19 +4,33 @@ import Core from '~/app/core';
 
 class AboutView extends Core.View {
   #modal;
+  #backdrop;
   #openButton;
+  #closeAreas;
 
   constructor(viewElement) {
     super(viewElement);
 
     this.#modal = $('.modal', viewElement);
+    this.#backdrop = $('.backdrop', viewElement);
     this.#openButton = $('.is-open-about', viewElement);
+
+    this.#closeAreas = $('.is-close-about, .backdrop');
+
+    this.#modal.display({ showClassName: 'is-shown' });
+    this.#backdrop.display({ showClassName: 'is-shown' });
 
     this.#listen();
   }
 
+  #displayModal(state) {
+    this.#modal.display(state);
+    this.#backdrop.display(state);
+  }
+
   #listen() {
-    this.#openButton.on('click', () => this.#modal.child().showModal());
+    this.#openButton.on('click', () => this.#displayModal(true));
+    this.#closeAreas.on('click', () => this.#displayModal(false));
   }
 }
 
